10e29af847755a142f14888e0de0674bf242b53f
teleivo <mrteleivo@gmail.com>
2016-03-14 10:34:23 +0100

    open mysql for acceptance tests
    * add user openmrs for external access
    * grant user openmrs@%
    * set mysqld bind-address

--- site/role/manifests/rispacs.pp
+++ site/role/manifests/rispacs.pp
@@ -4,14 +4,38 @@ class role::rispacs {
   include 'profile::packages'
   include 'mysql::server'
 
-  $openmrs_db_owner = hiera('openmrs::db_owner')
-  $openmrs_db_name = hiera('openmrs::db_name')
-  mysql_grant { "${openmrs_db_owner}@'%'/${openmrs_db_name}.*":
+  $openmrs_db_owner = hiera('openmrs::db_owner', 'openmrs')
+  $openmrs_db_owner_password = hiera('openmrs::db_owner_password', 'openmrs')
+  $openmrs_db_name = hiera('openmrs::db_name', 'openmrs')
+  $mysql_user_name_local = "${openmrs_db_owner}@localhost"
+  $mysql_user_name_all = "${openmrs_db_owner}@%"
+  mysql_user { $mysql_user_name_all:
+    ensure        => 'present',
+    password_hash => mysql_password($openmrs_db_owner_password),
+  }
+  mysql_grant { "${mysql_user_name_local}/*.*":
     ensure     => 'present',
     options    => ['GRANT'],
     privileges => ['ALL'],
-    table      => '*.*',
-    user       => "${openmrs_db_name}@'%'",
+    table      => "*.*",
+    user       => $mysql_user_name_local,
+    require    => Mysql_user[$mysql_user_name_local]
+  }
+  mysql_grant { "${mysql_user_name_all}/*.*":
+    ensure     => 'present',
+    options    => ['GRANT'],
+    privileges => ['ALL'],
+    table      => "*.*",
+    user       => $mysql_user_name_all,
+    require    => Mysql_user[$mysql_user_name_all]
+  }
+  mysql_grant { "${mysql_user_name_all}/${openmrs_db_name}.*":
+    ensure     => 'present',
+    options    => ['GRANT'],
+    privileges => ['ALL'],
+    table      => "${openmrs_db_name}.*",
+    user       => $mysql_user_name_all,
+    require    => Mysql_user[$mysql_user_name_all]
   }
 
   class { 'profile::java_oracle':
